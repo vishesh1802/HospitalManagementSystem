9ddc812babacc7883d8dd97abb4720268eefd9df
vishesh1802 <96537033+vishesh1802@users.noreply.github.com>
2025-05-12 18:33:22 -0500

    Delete note.py

--- note.py
+++
@@ -1,6 +0,0 @@
-class Note:
-    def __init__(self, note_id, note_type=None, visit_id=None, note_text=""):
-        self.note_id = note_id
-        self.note_type = note_type
-        self.visit_id = visit_id
-        self.note_text = note_text
